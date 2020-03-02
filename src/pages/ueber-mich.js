9e74cf73d3f16bae4236f9c1db7039d788047233
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-03-02 11:33:29 +0100

    fix spelling

--- src/pages/ueber-mich.js
+++ src/pages/ueber-mich.js
@@ -9,14 +9,19 @@ export default () => {
         <h2 className="text-xl py-4">Hallo liebe Interessierten,</h2>
         <div className="flex flex-wrap">
           <p className="md:w-6/12">
-            Heute sehen wir auf Internet, dass viele Menschen alles können und zwar bestens! Da scheint es manchmal unmöglich mitmachen zu können. Mir ist es wichtig den Spass vermitteln zu können. Wenn jemand Spass an der Sache hat fühlt er sich wohl, ist bereit mehr zu lernen en kreiert so einen sicheren Basis.
+            Heute sehen wir auf Internet, dass viele Menschen alles können und zwar bestens! Da scheint es manchmal unmöglich mitmachen zu können.
+            Mir ist es wichtig den Spass vermitteln zu können.
+            Wenn jemand Spass an der Sache hat fühlt er sich wohl, ist bereit mehr zu lernen und gestaltet so einen sicheren Basis.
           </p>
           <img className="md:w-6/12 inline-block object-contain" alt="schim logo" src={'/schwim.png'} />
         </div>
         <div className="flex flex-wrap">
           <img className="md:w-6/12 inline-block p-2 object-contain" alt="adinda im wasser" src={'/adinda-wasser.jpg'} />
           <p className="md:w-6/12">
-            Ich unternehme viel im sportlichen Bereich: Skitouren, Schlittschuhlaufen, Kajakfahren, Velofahren, Bergtouren und natürlich schwimmen. Bei all diese Sportarten ist das Konzept in Lernen gleich. Die persönliche Leistung steht im Schwimmen oft hoch angeschrieben, ich bin aber der Meinung, dass man die Leistung optimal geniessen kann, wenn es durch die Umgebung anerkannt ist, deshalb soll es einen Austausch geben die für alle Teilnehmern gleichwertig ist. Am schönsten ist es, wenn man lernt sich auch auf die andere Teilnehmer verlassen zu können, gemeinsam Ziele erreichen zu können und so ganz tolle Freundschaften und/oder Erinnerungen entstehen.
+            Ich unternehme viel im sportlichen Bereich: Skitouren, Schlittschuhlaufen, Kajakfahren, Velofahren, Bergtouren und natürlich schwimmen.
+            Bei all diese Sportarten ist das Konzept in Lernen gleich.
+            Die persönliche Leistung steht im Schwimmen oft hoch angeschrieben, ich bin aber der Meinung, dass man die Leistung optimal geniessen kann, wenn es durch die Umgebung anerkannt ist, deshalb soll es einen Austausch geben die für alle Teilnehmern gleichwertig ist.
+            Am schönsten ist es, wenn man lernt sich auch auf die andere Teilnehmer verlassen zu können, gemeinsam Ziele erreichen zu können und so ganz tolle Freundschaften und/oder Erinnerungen entstehen.
           </p>
         </div>
         <br />
